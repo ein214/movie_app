a692c10873d6af51321cd38856657f3c47777001
ein214 <einee214@gmail.com>
2020-05-12 17:29:56 +0900

    movie_app_20200512 - State

--- src/App.js
+++ src/App.js
@@ -1,68 +1,29 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-//props.fav = {fav}  같은 것
-function Food({ name, picture, rating }){  
-  //return <h1>I like {name}</h1>;
-  return (<div>
-    <h2>{name}</h2>
-    <h4>{rating}/5.0</h4>
-    <img src={picture} alt={name}/>
-  </div>)
-}
+class App extends React.Component {
+  state = {
+    count: 0
+  };
 
-const foodILike = [
-  {
-    id:1,
-    name: "Kimchi",
-    image:
-      "http://aeriskitchen.com/wp-content/uploads/2008/09/kimchi_bokkeumbap_02-.jpg",
-    rating : 5
-  },
-  {
-    id:2,
-    name: "Samgyeopsal",
-    image:
-      "https://3.bp.blogspot.com/-hKwIBxIVcQw/WfsewX3fhJI/AAAAAAAAALk/yHxnxFXcfx4ZKSfHS_RQNKjw3bAC03AnACLcBGAs/s400/DSC07624.jpg",
-    rating : 3
-  },
-  {
-    id:3,
-    name: "Bibimbap",
-    image:
-      "http://cdn-image.myrecipes.com/sites/default/files/styles/4_3_horizontal_-_1200x900/public/image/recipes/ck/12/03/bibimbop-ck-x.jpg?itok=RoXlp6Xb",
-    rating : 3.5
-  },
-  {
-    id:4,
-    name: "Doncasu",
-    image:
-      "https://s3-media3.fl.yelpcdn.com/bphoto/7F9eTTQ_yxaWIRytAu5feA/ls.jpg",
-    rating : 4.5
-  },
-  {
-    id:5,
-    name: "Kimbap",
-    image:
-      "http://cdn2.koreanbapsang.com/wp-content/uploads/2012/05/DSC_1238r-e1454170512295.jpg",
-    rating : 5
+  //setState를 이용해서 값을 바꿀때마다 다시 render 
+  //그냥 this.state.count로 값만 바꿨을때는 다시 redering 되지않음.
+  add = () => {
+    this.setState(current => ({ count: current.count + 1 }));
+  };
+  minus = () => {
+    this.setState(current => ({ count: current.count -1 }));
+  };
+  render() {
+    return (
+      <div>
+          <h1>The number is: {this.state.count}</h1>
+          <button onClick={this.add}>Add</button>
+          <button onClick={this.minus}>Minus</button>
+      </div>
+    )
   }
-];
 
-//xxx.propType = { } 저 부분은 꼭 propTypes 여야함 
-Food.propTypes = {
-  name: PropTypes.string.isRequired,
-  picture: PropTypes.string.isRequired,
-  rating: PropTypes.number.isRequired
-}
-
-function App() {
-  return (
-    <div>
-      <h1>Hello!!</h1>
-      {foodILike.map(dish => <Food name={dish.name} picture={dish.image} key={dish.id}  rating={dish.rating} />)}
-    </div>
-  );
 }
 
 export default App;
